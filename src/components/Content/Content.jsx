0eaf7cb856b4496be96dc5b7aab3e53e354a8330
KavinSatheeskumar <kavinsathees@gmail.com>
2022-12-23 20:08:50 -0500

    Add spacing to components on SOTS page

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import styles from './Content.module.css';
 
 // Text content with a title at the top
-const Content = ({ title, children }) => {
+const Content = ({ title, children, className }) => {
   return (
-    <div>
+    <div className={className}>
       <h1 className={styles.contentTitle}>{title}</h1>
       <div className={styles.contentText}>
         {children}
